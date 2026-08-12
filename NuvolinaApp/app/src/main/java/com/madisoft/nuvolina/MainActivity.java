26b67e56c36820309aaaac496c5bdea9bd758354
Ilalu <ilalu@Mini-di-Ilalu.lan>
2026-08-12 20:59:31 +0200

    Hide user profile and auto-redirect to compiti

--- NuvolinaApp/app/src/main/java/com/madisoft/nuvolina/MainActivity.java
+++ NuvolinaApp/app/src/main/java/com/madisoft/nuvolina/MainActivity.java
@@ -37,16 +37,35 @@ public class MainActivity extends Activity {
             public void onPageFinished(WebView view, String url) {
                 String js = "javascript:(function() { " +
                     "function hideUnwanted() {" +
-                    "  var menuItems = document.querySelectorAll('li, a, div[role=\"button\"]');" +
+                    "  var compitiLink = null;" +
+                    "  var menuItems = document.querySelectorAll('li, a, div[role=\"button\"], button, span, div');" +
                     "  menuItems.forEach(function(item) {" +
                     "    var text = item.textContent.trim();" +
                     "    if(text.length > 0 && text.length < 50) {" +
                     "      var isAllowed = text.indexOf('Compiti') !== -1 || text.indexOf('Argomenti') !== -1 || text.indexOf('Petra') !== -1 || text.indexOf('1B') !== -1;" +
-                    "      var isMenu = text.indexOf('Voti') !== -1 || text.indexOf('Assenze') !== -1 || text.indexOf('Note') !== -1 || text.indexOf('Calendario') !== -1 || text.indexOf('Colloqui') !== -1 || text.indexOf('Bacheche') !== -1 || text.indexOf('Documenti') !== -1 || text.indexOf('Pagamenti') !== -1 || text.indexOf('Modulistica') !== -1 || text.indexOf('Materiale') !== -1 || text.indexOf('Scrutinio') !== -1 || text.indexOf('Home') !== -1;" +
+                    "      var isMenu = text === 'Voti' || text.indexOf('Assenze') !== -1 || text.indexOf('Note') !== -1 || text.indexOf('Calendario') !== -1 || text.indexOf('Colloqui') !== -1 || text.indexOf('Bacheche') !== -1 || text.indexOf('Documenti') !== -1 || text.indexOf('Pagamenti') !== -1 || text.indexOf('Modulistica') !== -1 || text.indexOf('Materiale') !== -1 || text.indexOf('Scrutinio') !== -1 || text === 'Home';" +
                     "      if(isMenu && !isAllowed) { item.style.display = 'none'; }" +
+                    
+                    "      if(text === 'LUCA FEI' || text === 'LF') {" +
+                    "         var parent = item.closest('a, button, .dropdown, li');" +
+                    "         if(parent) { parent.style.display = 'none'; } else { item.style.display = 'none'; }" +
+                    "      }" +
+
+                    "      if(text.indexOf('Compiti') !== -1 && item.tagName.toLowerCase() === 'a' && item.href && item.href.indexOf('javascript') === -1) {" +
+                    "         compitiLink = item.href;" +
+                    "      }" +
                     "    }" +
                     "  });" +
+                    
+                    "  var currUrl = window.location.href.toLowerCase();" +
+                    "  if (compitiLink && currUrl.indexOf('/compiti') === -1 && currUrl.indexOf('/argomenti') === -1 && currUrl.indexOf('login') === -1) {" +
+                    "    if (!window.hasRedirectedToCompiti) {" +
+                    "       window.hasRedirectedToCompiti = true;" +
+                    "       window.location.replace(compitiLink);" +
+                    "    }" +
+                    "  }" +
                     "}" +
+                    
                     "hideUnwanted();" +
                     "if (!window.nuvolaObserverStarted) {" +
                     "  window.nuvolaObserverStarted = true;" +
@@ -55,7 +74,7 @@ public class MainActivity extends Activity {
                     "  setInterval(function() {" +
                     "    var curr = window.location.href.toLowerCase();" +
                     "    if(curr.indexOf('/voti')!==-1 || curr.indexOf('/assenze')!==-1 || curr.indexOf('/note')!==-1 || curr.indexOf('/pagamenti')!==-1 || curr.indexOf('/colloqui')!==-1 || curr.indexOf('/documenti')!==-1) {" +
-                    "      window.location.replace('https://nuvola.madisoft.it/login');" +
+                    "      window.location.replace('https://nuvola.madisoft.it/');" +
                     "    }" +
                     "  }, 1000);" +
                     "}" +
